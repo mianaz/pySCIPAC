b85f9cd3342aabd28edbcdc95ac834fad65cbed6
mianaz <zzeng4@nd.edu>
2025-12-19 11:58:18 -0500

    Export new AnnData workflow function

--- scipac/__init__.py
+++ scipac/__init__.py
@@ -3,10 +3,17 @@ SCIPAC: Single Cell Identifier of Phenotype-Associated Cells
 Python implementation of the SCIPAC R package
 """
 
-from .core import SCIPAC
+from .core import SCIPAC, run_scipac_anndata
 from .preprocessing import preprocess_sc_bulk_dat
 from .pca import sc_bulk_pca
 from .clustering import seurat_ct, leiden_clustering
 
 __version__ = "0.1.0"
-__all__ = ["SCIPAC", "preprocess_sc_bulk_dat", "sc_bulk_pca", "seurat_ct", "leiden_clustering"]
+__all__ = [
+    "SCIPAC",
+    "run_scipac_anndata",
+    "preprocess_sc_bulk_dat",
+    "sc_bulk_pca",
+    "seurat_ct",
+    "leiden_clustering",
+]
